3ab97cc2274e946583dce472d7f774e549a62e32
ImagineWagonsInSky <will18.perfect@gmail.com>
2025-06-30 19:25:33 +0200

    Run python file and write to file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,6 @@
 import os
 from constants import MAX_CHARS
+import subprocess
 
 def is_within_subdirectory(path, parent):
     path = os.path.abspath(path)
@@ -49,4 +50,53 @@ def get_file_content(working_directory, file_path):
 
     return file_content_string
 
+def write_file(working_directory, file_path, content):
+    file_path = os.path.join(working_directory, file_path)
 
+    if not is_within_subdirectory(file_path, working_directory):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    parent_dir = os.path.dirname(file_path)
+    if not os.path.exists(parent_dir):
+        os.makedirs(parent_dir)
+    try:
+        with open(file_path, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error writing to file: {file_path} {e}"
+
+def run_python_file(working_directory, file_path):
+    original_path_name = file_path
+    abs_working_dir = os.path.abspath(working_directory)
+    file_path = os.path.join(working_directory, file_path)
+
+    if not is_within_subdirectory(file_path, working_directory):
+        return f'Error: Cannot execute "{original_path_name}" as it is outside the permitted working directory'
+    if not os.path.exists(file_path):
+        return f'Error: File "{original_path_name}" not found.'
+    if not file_path.endswith(".py"):
+        return f'Error: "{original_path_name}" is not a Python file.'
+    
+    try:
+        result = subprocess.run(["python3",file_path], capture_output=True, timeout=30, text=True, cwd=abs_working_dir)
+        STDOUT = result.stdout
+        STDERR = result.stderr
+        RCODE = result.returncode
+        
+        return_string = ""
+        if STDOUT or STDERR:
+            if STDOUT:  
+                return_string += f"STDOUT: {STDOUT}"
+            if STDERR:
+                return_string += "\n" + f"STDERR: {STDERR}"
+        else:
+            return_string += "No output produced\n"
+
+        if RCODE != 0:
+            return_string += f"\nProcess exited with code {RCODE}"
+        
+        return return_string
+    
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,19 +1,28 @@
-from functions.get_files_info import get_files_info, get_file_content
+from functions.get_files_info import get_files_info, get_file_content, write_file, run_python_file
 
 
 def test():
-    result = get_file_content("calculator", "main.py")
-    print("Result for main.py:")
-    print(result)
-    print("")
+    # result = get_file_content("calculator", "main.py")
+    # print("Result for main.py:")
+    # print(result)
+    # print("")
 
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'pkg/calculator.py':")
-    print(result)
+    # result = get_file_content("calculator", "pkg/calculator.py")
+    # print("Result for 'pkg/calculator.py':")
+    # print(result)
 
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for '/bin/cat':")
-    print(result)
+    # result = get_file_content("calculator", "/bin/cat")
+    # print("Result for '/bin/cat':")
+    # print(result)
+
+    # print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    # print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    # print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
+    print(run_python_file("calculator", "main.py"))
+    print(run_python_file("calculator", "tests.py"))
+    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file("calculator", "nonexistent.py"))
 
 
 if __name__ == "__main__":
